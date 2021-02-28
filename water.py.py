6bd343dd64cc5414cc56f50b05612d7b635e18c5
Asharma002 <79785668+Asharma002@users.noreply.github.com>
2021-02-28 15:28:05 +0530

    Add files via upload

---
+++ water.py.py
@@ -0,0 +1,15 @@
+# from plyer import notification
+# import time
+# if __name__ == "__main__":
+#     while True:
+
+    
+#         notification.notify(
+
+#             title = "DRINK WATER",
+            
+#             message = "Water is essential for the kidneys and other bodily functions.When dehydrated, the skin can become more vulnerable to skin disorders and wrinkling.",
+#             app_icon =None,
+#             timeout =5
+#              )
+#         time.sleep(60)
